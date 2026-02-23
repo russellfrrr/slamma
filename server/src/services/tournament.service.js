7a0e91d043ad1ec8644de37bd1916ac9f1b8b87a
Russell Ferrero <russellferrero00@gmail.com>
2026-02-23 18:13:05 +0800

    feat: implement tournament services

---
+++ server/src/services/tournament.service.js
@@ -0,0 +1,30 @@
+import Tournament from '../models/Tournament.js';
+
+class TournamentService {
+  static async create(data) {
+    const existing = await Tournament.findOne({ slug: data.slug });
+    if (existing) {
+      throw new Error('Tournament with this slug already exists');
+    }
+
+    const tournament = Tournament.create(data);
+    return tournament;
+  }
+
+  static async getAll() {
+    const tournament = Tournament.find.sort({ createdAt: -1 });
+
+    return tournament;
+  }
+
+  static async getBySlug(slug) {
+    const tournament = await Tournament.findOne({ slug });
+    if (!tournament) {
+      throw new Error('Tournament not found');
+    }
+
+    return tournament;
+  }
+}
+
+export default TournamentService;
