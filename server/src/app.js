5f150053d384f711205b9f7feb78733b40775322
Russell Ferrero <russellferrero00@gmail.com>
2026-02-24 16:19:41 +0800

    feat: add cors dep on app server file

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import tournamentRouter from './routes/tournament.routes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
